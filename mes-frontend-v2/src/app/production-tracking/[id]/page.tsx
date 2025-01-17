dc501e0da615c5a5da960806dc1259e3a7d83386
ChristopherAhlim <christopher@mayvenr.com>
2025-01-18 04:33:57 +0800

    integrated get api for production detail page

--- mes-frontend-v2/src/app/production-tracking/[id]/page.tsx
+++ mes-frontend-v2/src/app/production-tracking/[id]/page.tsx
@@ -1,60 +1,104 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import BaseButton from "@/app/components/base-button";
+import { Production } from "@/interface/production";
+import { getOneProduction } from "@/services/production.service";
+import moment from "moment";
+import Link from "next/link";
 
 const ProductionTrackingDetailPage = () => {
   const params = useParams();
-  const pageId = params.id;
-  const data = {
-    productionId: pageId,
-    productionName: "Widget A",
-    productionStatus: "Completed",
-    startTime: "2023-10-01 08:30:00",
-    expectedEndTime: "2023-10-01 08:30:00",
-    actualEndTime: "2023-10-01 08:30:00",
-    productionQuantity: 150,
-  };
+  const pageId = Array.isArray(params.id) ? params.id[0] : params.id;
+
+  const [productionDetail, setProductionDetail] = useState<Production>();
 
   function statusColor(status: string) {
     switch (status) {
-      case "Completed":
-        return "bg-green-500";
-      case "In Progress":
-        return "bg-yellow-500";
-      case "Pending":
-        return "bg-red-500";
+      case "completed":
+        return "bg-green-600";
+      case "in_progress":
+        return "bg-orange-400";
+      case "cancelled":
+        return "bg-red-600";
+      case "planned":
+        return "bg-blue-600";
       default:
-        return "bg-gray-500";
+        return "bg-slate-600";
     }
   }
 
+  function mapStatusText(status: string) {
+    switch (status) {
+      case "completed":
+        return "COMPLETED";
+      case "in_progress":
+        return "IN PROGRESS";
+      case "cancelled":
+        return "CANCELLED";
+      case "planned":
+        return "PLANNED";
+      default:
+        return "UNDEFINED";
+    }
+  }
+
+  async function initProductionDetail() {
+    try {
+      const res = await getOneProduction(parseInt(pageId));
+      setProductionDetail(res[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    initProductionDetail();
+  }, []);
+
   return (
     <div className="w-full">
-      <div className="text-3xl font-bold mt-5 mb-8">
+      <div className="pt-4">
+        <Link href={"/production-tracking"}>{"< Go Back"}</Link>
+      </div>
+      <div className="text-3xl font-bold mt-3 mb-8">
         Production Tracking Details
       </div>
 
       <div className="w-[90%] sm:w-[70%] mx-auto border-2 rounded-md border-slate-300 grid grid-cols-1 gap-8 p-8 sm:grid-cols-2">
-        <DetailItem label="Production Id">{data.productionId}</DetailItem>
-        <DetailItem label="Production Name">{data.productionName}</DetailItem>
+        <DetailItem label="Production Id">{productionDetail?.id}</DetailItem>
+        <DetailItem label="Production Name">
+          {productionDetail?.product_name}
+        </DetailItem>
         <DetailItem label="Status">
           <div
             className={`px-4 py-1 text-white rounded-full w-fit ${statusColor(
-              data.productionStatus
+              productionDetail?.status || "undefined"
             )} `}
           >
-            {data.productionStatus}
+            {mapStatusText(productionDetail?.status || "undefined")}
           </div>
         </DetailItem>
         <DetailItem label="Production Quantity">
-          {data.productionQuantity}
+          {productionDetail?.quantity}
         </DetailItem>
-        <DetailItem label="Start Time">{data.startTime}</DetailItem>
+        <DetailItem label="Start Time">
+          {moment(productionDetail?.start_date).format("DD MMMM YYYY h:mm A") ||
+            "-"}
+        </DetailItem>
         <DetailItem label="Expected End Time">
-          {data.expectedEndTime}
+          {moment(productionDetail?.expected_end_date).format(
+            "DD MMMM YYYY h:mm A"
+          ) || "-"}
         </DetailItem>
-        <DetailItem label="Actual End Time">{data.actualEndTime}</DetailItem>
+        <DetailItem label="Actual End Time">
+          {moment(productionDetail?.actual_end_date).format(
+            "DD MMMM YYYY h:mm A"
+          ) || "-"}
+        </DetailItem>
+        <DetailItem label="Remarks">
+          {productionDetail?.remarks || "-"}
+        </DetailItem>
       </div>
       <div className="flex justify-between w-[90%] sm:w-[70%] mx-auto mt-8">
         <BaseButton style="filled" className="w-[40%] font-bold">
